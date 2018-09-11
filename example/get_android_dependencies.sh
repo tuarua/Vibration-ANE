bd225e56ae75d008e9a130b7426251686b52707a
Eoin Landy <info@tuarua.co.uk>
2018-09-12 00:05:35 +0100

    0.0.7 ready to merge

--- example/get_android_dependencies.sh
+++ example/get_android_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 wget -O android_dependencies/com.tuarua.frekotlin.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin.ane?raw=true
-wget -O ../native_extension/ane/VibrationANE.ane https://github.com/tuarua/Vibration-ANE/releases/download/0.0.6/VibrationANE.ane?raw=true
+wget -O ../native_extension/ane/VibrationANE.ane https://github.com/tuarua/Vibration-ANE/releases/download/0.0.7/VibrationANE.ane?raw=true
