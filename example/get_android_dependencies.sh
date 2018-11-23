33bb9c3fd1dc05a2270ed958c90851368dd8006d
Tua Rua <info@tuarua.co.uk>
2018-11-23 22:06:02 +0000

    Update get_android_dependencies.sh

--- example/get_android_dependencies.sh
+++ example/get_android_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 wget -O android_dependencies/com.tuarua.frekotlin.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin.ane?raw=true
-wget -O ../native_extension/ane/VibrationANE.ane https://github.com/tuarua/Vibration-ANE/releases/download/0.0.8/VibrationANE.ane?raw=true
+wget -O ../native_extension/ane/VibrationANE.ane https://github.com/tuarua/Vibration-ANE/releases/download/0.1.0/VibrationANE.ane?raw=true
