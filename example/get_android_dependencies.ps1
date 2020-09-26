d4921daf1aea2c97600f15d32e5d810a2f8be3d6
Eoin Landy <info@tuarua.co.uk>
2020-09-26 11:34:48 +0100

    use air-tools

--- example/get_android_dependencies.ps1
+++
@@ -1,7 +0,0 @@
-$AneVersion = "1.5.0"
-$FreKotlinVersion = "1.40.0"
-
-$currentDir = (Get-Item -Path ".\" -Verbose).FullName
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Vibration-ANE/releases/download/$AneVersion/VibrationANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\VibrationANE.ane"
-Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.tuarua.frekotlin-$FreKotlinVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
--- example/get_android_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-AneVersion="1.5.0"
-FreKotlinVersion="1.40.0"
-
-wget -O android_dependencies/com.tuarua.frekotlin-$FreKotlinVersion.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
-wget -O ../native_extension/ane/VibrationANE.ane https://github.com/tuarua/Vibration-ANE/releases/download/$AneVersion/VibrationANE.ane?raw=true
